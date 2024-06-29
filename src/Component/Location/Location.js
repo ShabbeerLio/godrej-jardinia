275870421e1d050708895857c2f89f3d56fed80a
shabbeer <mdshabbeer707@gmail.com>
2024-06-29 18:30:43 +0530

    copy right removed

--- src/Component/Location/Location.js
+++ src/Component/Location/Location.js
@@ -8,7 +8,7 @@ const Location = () => {
                 <h3>Location</h3>
             </div>
             <div className="location-box">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3507.297727636259!2d77.45119567536406!3d28.470579091416866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce9ef0e837dc5%3A0x3142a2640c89e38c!2sGodrej%20Jardinia%2C%20146%20Sector%2C%20Noida!5e0!3m2!1sen!2sin!4v1715339137014!5m2!1sen!2sin"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3507.2994576530423!2d77.45123127549492!3d28.47052697575353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce9a13d3f6a61%3A0x451ed1c0b331b11e!2sJardinia%20Noida!5e0!3m2!1sen!2sin!4v1719665923040!5m2!1sen!2sin"></iframe>
             </div>
         </div>
     )
